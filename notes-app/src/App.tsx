17d648e6259dcdfd5f71d815d6dd427ea55aade2
Femolala <you@example.com0?>
2025-11-07 17:11:22 +0100

    build issues

--- notes-app/src/App.tsx
+++ notes-app/src/App.tsx
@@ -1,24 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
-
-
-const routes = (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Signup />} />
-    </Routes>
-  </Router>
-);
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
+  const token = localStorage.getItem("token");
+
   return (
-    <div>
-      {routes}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
+        
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
